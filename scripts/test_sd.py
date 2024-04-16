7451fed8a70c0dcbb675cd65bcac37c17a551c24
shadeofblue <blue@golem.network>
2024-04-16 19:02:03 +0200

    use actors

--- scripts/test_sd.py
+++ scripts/test_sd.py
@@ -1,19 +1,42 @@
 import argparse
 from datetime import datetime, timezone
 
+import ray
+
 from diffusers import StableDiffusionPipeline
+
+
+@ray.remote(num_gpus=1.0)
+class Renderer:
+    def __init__(self):
+        pipe = StableDiffusionPipeline.from_pretrained("./stable-diffusion-v1-5")
+        self.pipe = pipe.to("cuda")
+
+    def render_image(self, i: int, prompt: str):
+        img = self.pipe(prompt).images[0]
+        return i, img
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("prompt")
+parser.add_argument("-n", "--num-images", type=int, default=1)
 args = parser.parse_args()
 
-pipe = StableDiffusionPipeline.from_pretrained("./stable-diffusion-v1-5")
-pipe = pipe.to("cuda")
-
-img = pipe(args.prompt).images[0]
+prompt: str = args.prompt
 
 current_time_str = datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S%z")
-filename = f"out-{current_time_str}.jpg"
+filename = f"{prompt.replace(' ', '-')}-{current_time_str}"
 
-print(filename)
-img.save(filename)
+ray.init()
+
+renderer = Renderer.remote()
+
+results = [renderer.render_image.remote(i, args.prompt) for i in range(args.num_images)]
+
+while results:
+    ready, results = ray.wait(results, num_returns=1)
+    for r in ready:
+        i, img = ray.get(r)
+        fn = f"{filename}-{i}.jpg"
+        print(fn)
+        img.save(fn)
