9cbd83a371cb1a2504f8bb5991b71827d5d6c0aa
shadeofblue <blue@golem.network>
2024-04-22 16:13:44 +0200

    better multitasking

--- scripts/test_sd.py
+++ scripts/test_sd.py
@@ -12,9 +12,9 @@ class Renderer:
         pipe = StableDiffusionPipeline.from_pretrained("./stable-diffusion-v1-5")
         self.pipe = pipe.to("cuda")
 
-    def render_image(self, i: int, prompt: str):
+    def render_image(self, idx: int, i: int, prompt: str):
         img = self.pipe(prompt).images[0]
-        return i, img
+        return idx, i, img
 
 
 parser = argparse.ArgumentParser()
@@ -29,14 +29,30 @@ filename = f"{prompt.replace(' ', '-')}-{current_time_str}"
 
 ray.init()
 
-renderer = Renderer.remote()
+num_gpus = int(ray.cluster_resources().get("GPU"))
+print("Available GPUs: ", num_gpus, "vs images to render: ", args.num_images)
 
-results = [renderer.render_image.remote(i, args.prompt) for i in range(args.num_images)]
+renderers = [Renderer.remote() for _ in range(num_gpus)]
+renderers_available = [i for i in range(len(renderers))]
 
-while results:
+img_cnt = 0
+img_done = 0
+
+results = []
+
+while img_done < args.num_images:
+    for ridx in renderers_available:
+        renderers_available.remove(ridx)
+        if img_cnt < args.num_images:
+            print("scheduling: ", img_cnt, "on: ", ridx)
+            results.append(renderers[ridx].render_image.remote(ridx, img_cnt, args.prompt))
+        img_cnt += 1
+
     ready, results = ray.wait(results, num_returns=1)
     for r in ready:
-        i, img = ray.get(r)
+        ridx, i, img = ray.get(r)
+        renderers_available.append(ridx)
         fn = f"{filename}-{i}.jpg"
         print(fn)
         img.save(fn)
+        img_done += 1
